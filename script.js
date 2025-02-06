27b2de784d46ada05a2a5e201788cae81cd3c0ca
nirvana-siw <neeravhmakwana123@gmail.com>
2025-02-06 20:35:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
+    "Are you sure mam?",
+    "Really sure MJ??",
     "Are you positive?",
-    "Pookie please...",
+    "Bacchuu please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
+    "I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Pem nahi khane dunga...",
+    "Kissi kar dunga sabke saamne! ❤️"
 ];
 
 let messageIndex = 0;
